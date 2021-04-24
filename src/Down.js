b29fb1e3a66a3e1b549343b12e15f8866304abc7
YashPSRN7 <56504822+YashPSRN7@users.noreply.github.com>
2021-04-24 20:41:10 +0530

    added pages and router : status : Not Working

---
+++ src/Down.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Down extends React.Component{
+    render() {
+       return (
+           <div className="Down">
+               <h1>hello</h1>
+           </div>
+       )
+    }
+}
+export default Down;
