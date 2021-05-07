456b9205077dbfd543476ec12f7c6f71b7e3540d
Pawan <me@pawanpujari.com>
2021-05-08 01:38:18 +0530

    Major changes, chatbot added, need to change css, still

--- src/App.js
+++ src/App.js
@@ -5,9 +5,89 @@ import News from "./components/News";
 import Down from "./components/Down";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-import Learn from "./components/Learn.jsx"
+import Learn from "./components/Learn.jsx";
+import { TextField, Button } from "@material-ui/core";
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import "./App.css";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleQuery = this.handleQuery.bind(this);
+        this.handleQuerySubmit = this.handleQuerySubmit.bind(this);
+        this.handleNewUserMessage = this.handleNewUserMessage.bind(this);
+        this.state = { response: "", query: "" };
+    }
+
+    componentDidMount() {
+        // this.setState({ temp: tempVar });
+
+        console.log("SPAMSPAMSPAMSPAMS");
+        // console.log(tempVar);
+        fetch("/test", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({
+                query: "bruh",
+            }),
+        });
+    }
+
+    handleQuery(e) {
+        e.preventDefault();
+
+        // if (e.key === "Enter") {
+        //     console.log(e.target.value);
+        // }
+        this.setState({ query: e.target.value });
+    }
+
+    handleQuerySubmit() {
+        fetch("/test", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({
+                query: this.state.query,
+            }),
+        });
+
+        fetch("/time")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ response: data.response });
+            });
+    }
+
+    // For widget
+    handleNewUserMessage(q) {
+        fetch("/test", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({
+                query: q,
+            }),
+        });
+
+        fetch("/time")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ response: data.response }, () => {
+                    console.log(this.state.response);
+                    addResponseMessage(this.state.response);
+                });
+            });
+    }
+
     render() {
         return (
             <Router>
@@ -15,6 +95,39 @@ class App extends Component {
                     {/* Insert Logo here */}
 
                     <Navbar> </Navbar>
+                    {/* <TextField
+                        id="outlined-basic"
+                        label="Outlined"
+                        variant="outlined"
+                        onChange={this.handleQuery}
+                        style={{
+                            backgroundColor: "white",
+                        }}
+                        InputProps={{
+                            style: {
+                                color: "black",
+                            },
+                        }}
+                    />
+
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleQuerySubmit}
+                    >
+                        Ask
+                    </Button>
+
+                    <p style={{ color: "white" }}>
+                        {" "}
+                        FinBot Says : {this.state.response}{" "}
+                    </p> */}
+
+                    <Widget
+                        title={"FinBot"}
+                        subtitle={"For all your financial needs"}
+                        handleNewUserMessage={this.handleNewUserMessage}
+                    />
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route path="/news" component={News} />
--- src/components/SectionSlide.jsx
+++ src/components/SectionSlide.jsx
@@ -3,11 +3,9 @@ import { Controller, Scene } from "react-scrollmagic";
 import { Tween, Timeline } from "react-gsap";
 import "./SectionSlide.css";
 import Logo from "../media/finlogo.png";
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Image from 'react-bootstrap/Image'
+// import Jumbotron from 'react-bootstrap/Jumbotron'
+// import Image from 'react-bootstrap/Image'
 // import  Grid  from "react-bootstrap/Grid";
-
-
 
 const SectionSlide = () => (
     <div className="sectionwipes">
@@ -15,88 +13,167 @@ const SectionSlide = () => (
             <Scene triggerHook="onLeave" duration="300%" pin>
                 <Timeline wrapper={<div id="pinContainer" />}>
                     <section className="panel blue">
-                        
-                           <img
-                                className="Logo2"
-                                src={Logo}
-                                alt="potato internet, kya kare hum"
-                            />
-                            <h1 className="bruh">Our Developers</h1>
-                        
+                        <img
+                            className="Logo2"
+                            src={Logo}
+                            alt="potato internet, kya kare hum"
+                        />
+                        <h1 className="bruh">Our Developers</h1>
                     </section>
                     <Tween from={{ x: "-100%" }} to={{ x: "0%" }}>
                         <section className="panel turqoise">
-                            
-                                <div className="container bootstrap snippets bootdey">
-                                        <div className="profile card2">
-                                        <div className="profile-body">
-                                            <div className="profile-bio">
+                            <div className="container bootstrap snippets bootdey">
+                                <div className="profile card2">
+                                    <div className="profile-body">
+                                        <div className="profile-bio">
                                             <div className="row">
                                                 <div className="col-md-5 text-center">
-                                                <img className="img-thumbnail md-margin-bottom-10" src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg" alt="" />
+                                                    <img
+                                                        className="img-thumbnail md-margin-bottom-10"
+                                                        src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg"
+                                                        alt=""
+                                                    />
                                                 </div>
                                                 <div className="col-md-7">
-                                                <h2>Edward Rooster</h2>
-                                                <h3>Contirbution</h3>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
-                                                <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
-                                                     
+                                                    <h2>Edward Rooster</h2>
+                                                    <h3>Contirbution</h3>
+                                                    <p>
+                                                        Lorem ipsum dolor sit
+                                                        amet, consectetur
+                                                        adipiscing elit.
+                                                        Suspendisse eget massa
+                                                        nec turpis congue
+                                                        bibendum. Integer nulla
+                                                        felis, porta suscipit
+                                                        nulla et, dignissim
+                                                        commodo nunc. Morbi a
+                                                        semper nulla.
+                                                    </p>
+                                                    <p>
+                                                        Proin mauris odio,
+                                                        pharetra quis ligula
+                                                        non, vulputate vehicula
+                                                        quam. Nunc in libero
+                                                        vitae nunc ultricies
+                                                        tincidunt ut sed leo.
+                                                        Sed luctus dui ut congue
+                                                        consequat. Cras
+                                                        consequat nisl ante, nec
+                                                        malesuada velit
+                                                        pellentesque ac.
+                                                        Pellentesque nec arcu in
+                                                        ipsum iaculis convallis.
+                                                    </p>
                                                 </div>
-                                            </div>    
                                             </div>
                                         </div>
-                                        </div>
                                     </div>
-                            
+                                </div>
+                            </div>
                         </section>
                     </Tween>
                     <Tween from={{ x: "100%" }} to={{ x: "0%" }}>
                         <section className="panel green">
                             <div className="container bootstrap snippets bootdey">
-                                        <div className="profile card2">
-                                        <div className="profile-body">
-                                            <div className="profile-bio">
+                                <div className="profile card2">
+                                    <div className="profile-body">
+                                        <div className="profile-bio">
                                             <div className="row">
                                                 <div className="col-md-5 text-center">
-                                                <img className="img-thumbnail md-margin-bottom-10" src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg" alt="" />
+                                                    <img
+                                                        className="img-thumbnail md-margin-bottom-10"
+                                                        src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg"
+                                                        alt=""
+                                                    />
                                                 </div>
                                                 <div className="col-md-7">
-                                                <h2>Edward Rooster</h2>
-                                                <h3>Contirbution</h3>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
-                                                <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
-                                                     
-                                                        
+                                                    <h2>Edward Rooster</h2>
+                                                    <h3>Contirbution</h3>
+                                                    <p>
+                                                        Lorem ipsum dolor sit
+                                                        amet, consectetur
+                                                        adipiscing elit.
+                                                        Suspendisse eget massa
+                                                        nec turpis congue
+                                                        bibendum. Integer nulla
+                                                        felis, porta suscipit
+                                                        nulla et, dignissim
+                                                        commodo nunc. Morbi a
+                                                        semper nulla.
+                                                    </p>
+                                                    <p>
+                                                        Proin mauris odio,
+                                                        pharetra quis ligula
+                                                        non, vulputate vehicula
+                                                        quam. Nunc in libero
+                                                        vitae nunc ultricies
+                                                        tincidunt ut sed leo.
+                                                        Sed luctus dui ut congue
+                                                        consequat. Cras
+                                                        consequat nisl ante, nec
+                                                        malesuada velit
+                                                        pellentesque ac.
+                                                        Pellentesque nec arcu in
+                                                        ipsum iaculis convallis.
+                                                    </p>
                                                 </div>
-
-                                            </div>    
                                             </div>
                                         </div>
-                                        </div>
                                     </div>
+                                </div>
+                            </div>
                         </section>
                     </Tween>
                     <Tween from={{ y: "-100%" }} to={{ y: "0%" }}>
                         <section className="panel bordeaux">
                             <div className="container bootstrap snippets bootdey">
-                                        <div className="profile card2">
-                                        <div className="profile-body">
-                                            <div className="profile-bio">
+                                <div className="profile card2">
+                                    <div className="profile-body">
+                                        <div className="profile-bio">
                                             <div className="row">
                                                 <div className="col-md-5 text-center">
-                                                <img className="img-thumbnail md-margin-bottom-10" src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg" alt="" />
+                                                    <img
+                                                        className="img-thumbnail md-margin-bottom-10"
+                                                        src="https://i.pinimg.com/736x/91/7d/6f/917d6fe868189926002ca0db7e80ccd4.jpg"
+                                                        alt=""
+                                                    />
                                                 </div>
                                                 <div className="col-md-7">
-                                                <h2>Edward Rooster</h2>
-                                                <h3>Contirbution</h3>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
-                                                <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                                                    <h2>Edward Rooster</h2>
+                                                    <h3>Contirbution</h3>
+                                                    <p>
+                                                        Lorem ipsum dolor sit
+                                                        amet, consectetur
+                                                        adipiscing elit.
+                                                        Suspendisse eget massa
+                                                        nec turpis congue
+                                                        bibendum. Integer nulla
+                                                        felis, porta suscipit
+                                                        nulla et, dignissim
+                                                        commodo nunc. Morbi a
+                                                        semper nulla.
+                                                    </p>
+                                                    <p>
+                                                        Proin mauris odio,
+                                                        pharetra quis ligula
+                                                        non, vulputate vehicula
+                                                        quam. Nunc in libero
+                                                        vitae nunc ultricies
+                                                        tincidunt ut sed leo.
+                                                        Sed luctus dui ut congue
+                                                        consequat. Cras
+                                                        consequat nisl ante, nec
+                                                        malesuada velit
+                                                        pellentesque ac.
+                                                        Pellentesque nec arcu in
+                                                        ipsum iaculis convallis.
+                                                    </p>
                                                 </div>
-                                            </div>    
                                             </div>
                                         </div>
-                                        </div>
                                     </div>
+                                </div>
+                            </div>
                         </section>
                     </Tween>
                 </Timeline>
